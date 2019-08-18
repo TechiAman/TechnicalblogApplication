beffa091d50da8d3f0ac94989bb1df31b36dc9ef
Abhishek Aman <abhishek.aman@trakinvest.com>
2019-08-18 15:26:01 +0530

    completed the create blog feature

--- src/main/java/technicalblog/controller/UserController.java
+++ src/main/java/technicalblog/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import technicalblog.model.User;
 import technicalblog.service.PostService;
+import technicalblog.service.UserService;
 
 @Controller
 public class UserController {
@@ -14,25 +15,37 @@ public class UserController {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("/users/login")
     public String login(){
         return "users/login";
     }
 
-    @RequestMapping("/users/registration")
-    public String registration(){
-        return "users/registration";
+    @RequestMapping(value = "/users/login", method=RequestMethod.POST)
+    public String loginUser(User user) {
+        if(userService.login(user)) {
+            return "redirect:/posts";
+        }
+        else {
+            return "users/login";
+        }
     }
-
-    @RequestMapping(value = "/users/login", method = RequestMethod.POST)
-    public String loginUser(User user){
-        return "redirect:/posts";
-    }
-
     @RequestMapping(value = "/users/logout", method = RequestMethod.POST)
     public String logout(Model model){
 
         model.addAttribute("posts",this.postService.getAllPosts());
         return "index";
+    }
+
+    @RequestMapping("/users/registration")
+    public String registration() {
+        return "users/registration";
+    }
+
+    @RequestMapping(value = "/users/registration", method=RequestMethod.POST)
+    public String registerUser(User user) {
+        return "users/login";
     }
 }
