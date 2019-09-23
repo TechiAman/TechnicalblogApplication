30978e35cc9dfe858943858bfb0b1117f57dc545
Abhishek Aman <abhishek.aman@trakinvest.com>
2019-09-23 12:58:47 +0530

    added JPA configuration to connect to the database & removed JDBC implementation

--- src/main/java/technicalblog/service/PostService.java
+++ src/main/java/technicalblog/service/PostService.java
@@ -3,61 +3,28 @@ package technicalblog.service;
 import org.springframework.stereotype.Service;
 import technicalblog.model.Post;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import javax.persistence.TypedQuery;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class PostService {
 
-    public ArrayList<Post> getAllPosts(){
-        ArrayList<Post> posts= new ArrayList<>();
-        /*Post post = new Post();
-        post.setTitle("Post 1");
-        post.setBody("Post Body 1");
-        post.setDate(new Date());
+    @PersistenceUnit (unitName = "techblog")
+    private EntityManagerFactory entityManagerFactory;
 
-        Post post2 = new Post();
-        post2.setTitle("Post 2");
-        post2.setBody("Post Body 2");
-        post2.setDate(new Date());
+    public List<Post> getAllPosts(){
 
-        Post post3 = new Post();
-        post3.setTitle("Post 3");
-        post3.setBody("Post Body 3");
-        post3.setDate(new Date());
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-        posts.add(post);
-        posts.add(post2);
-        posts.add(post3);*/
+        TypedQuery<Post> query = entityManager.createQuery("SELECT p from Post p",Post.class);
+        List<Post> posts= query.getResultList();
 
-        Connection connection = null;
-        try {
-            Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/technicalblog","postgres","trak@123");
-            Statement statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery("SELECT * FROM posts");
-            while(rs.next()){
-                Post post = new Post();
-                post.setTitle(rs.getString("title"));
-                post.setBody(rs.getString("body"));
-                posts.add(post);
-            }
-
-
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        catch (SQLException e) {
-            e.printStackTrace();
-        }
-        finally {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
         return posts;
     }
 
